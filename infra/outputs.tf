c6f564c9f52dfc07f44cac8ca7af97953a5f59c1
tran-haison <haison250199@gmail.com>
2025-10-04 00:05:50 +0700

    fix: remove resource group created by opentofu

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,12 +1,12 @@
-# Resource Group Output
+# Resource Group Output (manually created)
 output "resource_group_name" {
-  description = "Name of the Azure Resource Group"
-  value       = azurerm_resource_group.my_resource_group.name
+  description = "Name of the manually created Azure Resource Group"
+  value       = var.prefix
 }
 
 output "resource_group_location" {
-  description = "Location of the Azure Resource Group"
-  value       = azurerm_resource_group.my_resource_group.location
+  description = "Location of the manually created Azure Resource Group"
+  value       = var.location
 }
 
 # AKS Cluster Outputs
